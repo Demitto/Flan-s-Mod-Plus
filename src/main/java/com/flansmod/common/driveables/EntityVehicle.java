79febb935d92ffd9b9cddfb811f8ce76f248f4a9
PrototypeTheta <prototypetheta@yahoo.co.uk>
2016-12-22 21:22:23 +0000

    I am incompetent

--- src/main/java/com/flansmod/common/driveables/EntityVehicle.java
+++ src/main/java/com/flansmod/common/driveables/EntityVehicle.java
@@ -42,7 +42,6 @@ import com.flansmod.common.guns.ItemBullet;
 import com.flansmod.common.guns.raytracing.BulletHit;
 import com.flansmod.common.network.PacketDriveableKey;
 import com.flansmod.common.network.PacketDriveableKeyHeld;
-import com.flansmod.common.network.PacketIT1Reload;
 import com.flansmod.common.network.PacketParticle;
 import com.flansmod.common.network.PacketPlaySound;
 import com.flansmod.common.network.PacketVehicleControl;
